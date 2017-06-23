0cce408e1ecfb37ebe5e194875ff594c2f60eacf
APADMASHRIKARTHIKA <padhmashrisabeen@gmail.com>
2017-06-23 10:08:45 +0530

    Create Primenum.java

---
+++ Primenum.java
@@ -0,0 +1,39 @@
+package set;
+
+import java.util.Scanner;
+
+public class primenumber {
+	public static void main(String[] args)
+	{
+		int n,m,i = 0;
+		System.out.println("Enter the number m");
+		Scanner s1=new Scanner(System.in);
+		m= s1.nextInt();
+		System.out.println("Enter the number n");
+		Scanner s2=new Scanner(System.in);
+		n= s2.nextInt();
+		for(i = m; i <= n; i++)
+        {
+            int flag = 0;
+			for( int j = 2; j < i; j++)
+            {
+                if(i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+                }
+            }
+            if(flag == 1)
+            {
+                System.out.println(i);
+            }
+		
+		
+	}
+
+}
+}
