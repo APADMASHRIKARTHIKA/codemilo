e1643e2bf14fa21b3a5985a68e5177da4b612f60
APADMASHRIKARTHIKA <padhmashrisabeen@gmail.com>
2017-06-23 10:02:27 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,24 @@
+package set;
+
+import java.util.Scanner;
+
+public class odd {
+	public static void main(String[] args)
+	{
+		int n,m,i = 0;
+		System.out.println("Enter the number m");
+		Scanner s1=new Scanner(System.in);
+		m= s1.nextInt();
+		System.out.println("ENter the number n");
+		Scanner s2=new Scanner(System.in);
+		n= s2.nextInt();
+		for(i=m;i<=n;i++)
+		{
+		if(i%2!=0)
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
